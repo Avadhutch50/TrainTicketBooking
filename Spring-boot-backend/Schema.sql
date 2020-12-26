7ad8656155be39607a68409546989f688cb33838
Avadhoot Chavan <avadhutch50@gmail.com>
2020-12-26 15:22:57 +0530

    Add files via upload

---
+++ Spring-boot-backend/Schema.sql
@@ -0,0 +1,23 @@
+ insert into train_details (train_number,source,destination,details,train_name) 
+ values(3920,'Delhi','Mumbai','Departs on Sunday and Friday','Rajdhani Express')
+ 
+ insert into train_details (train_number,source,destination,details,train_name) 
+ values(1003,'Howda','Kanyakumari','Departs on Monday only','Duranto Express')
+ 
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(2890,'Coimbatore','Jabalpur','Departs daily','Coimbatore Jabalpur Super Fast Express')
+ 
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(5012,'Mumbai','Pune','Departs daily','Mumbai - Pune Express')
+ 
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(8909,'Madgoan','Mumbai','Departs daily','Konkan Kany a Express')
+
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(1029,'Ahmedabad','Mumbai','Departs daily','Duronto Express')
+
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(1039,'Mumbai','Delhi','Departs on Sunday and Friday','Rajdhani Express')
+ 
+ insert into train_details(train_number,source,destination,details,train_name) 
+ values(2003,'Delhi','Ahmedabad','Departs on Tuesday','Happa Express')
